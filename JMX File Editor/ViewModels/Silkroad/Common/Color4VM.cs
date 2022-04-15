3f0e80f8da4f0f8071ae35c8bb6fd6c4030445a5
Engels Quintero <engelsquintero@gmail.com>
2022-04-15 14:26:28 -0400

    Fixed missing override

--- JMX File Editor/ViewModels/Silkroad/Common/Color4VM.cs
+++ JMX File Editor/ViewModels/Silkroad/Common/Color4VM.cs
@@ -3,6 +3,7 @@ namespace JMXFileEditor.ViewModels.Silkroad.Common
 {
 	public class Color4VM : JMXStructure
 	{
+		#region Constructor
 		public Color4VM(string Name, Color4 Color) : base(Name, true)
 		{
 			Childs.Add(new JMXAttribute("Red", Color.Red));
@@ -10,5 +11,19 @@ namespace JMXFileEditor.ViewModels.Silkroad.Common
 			Childs.Add(new JMXAttribute("Blue", Color.Blue));
 			Childs.Add(new JMXAttribute("Alpha", Color.Alpha));
 		}
+		#endregion
+
+		#region Public Methods
+		public override object GetClassFrom(JMXStructure Structure)
+		{
+			return new Color4()
+			{
+				Red = (float)((JMXAttribute)Structure.Childs[0]).Value,
+				Green = (float)((JMXAttribute)Structure.Childs[1]).Value,
+				Blue = (float)((JMXAttribute)Structure.Childs[2]).Value,
+				Alpha = (float)((JMXAttribute)Structure.Childs[3]).Value
+			};
+		}
+		#endregion
 	}
 }
