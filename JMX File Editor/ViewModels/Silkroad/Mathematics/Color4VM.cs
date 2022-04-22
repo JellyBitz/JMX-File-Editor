e5d34350305f57feb0f24c5c90bd7745082d2331
Engels Quintero <engelsquintero@gmail.com>
2022-04-22 06:16:54 -0400

    Added new application icon Added demo color to ui

--- JMX File Editor/ViewModels/Silkroad/Mathematics/Color4VM.cs
+++ JMX File Editor/ViewModels/Silkroad/Mathematics/Color4VM.cs
@@ -1,16 +1,44 @@
 ﻿using JMXFileEditor.Silkroad.Mathematics;
+using System.ComponentModel;
+using System.Windows.Media;
 
 namespace JMXFileEditor.ViewModels.Silkroad.Mathematics
 {
     public class Color4VM : JMXStructure
     {
+        #region Public Properties
+        public SolidColorBrush DemoBrush { get; private set; }
+        #endregion
+
         #region Constructor
         public Color4VM(string Name, Color4 Color) : base(Name, true)
         {
-            Childs.Add(new JMXAttribute("Red", Color.Red));
-            Childs.Add(new JMXAttribute("Green", Color.Green));
-            Childs.Add(new JMXAttribute("Blue", Color.Blue));
+            var red = new JMXAttribute("Red", Color.Red);
+            var green = new JMXAttribute("Green", Color.Green);
+            var blue = new JMXAttribute("Blue", Color.Blue);
+            // Add nodes
+            Childs.Add(red);
+            Childs.Add(green);
+            Childs.Add(blue);
             Childs.Add(new JMXAttribute("Alpha", Color.Alpha));
+            // Add handlers to update color brush
+            void OnColorChange(object s, PropertyChangedEventArgs e)
+            {
+                DemoBrush = new SolidColorBrush(new Color()
+                {
+                    R = (byte)((float)red.Value * 255),
+                    G = (byte)((float)green.Value * 255),
+                    B = (byte)((float)blue.Value * 255),
+                    A = 255
+                });
+                ;
+                OnPropertyChanged(nameof(DemoBrush));
+            };
+            red.PropertyChanged += OnColorChange;
+            green.PropertyChanged += OnColorChange;
+            blue.PropertyChanged += OnColorChange;
+            // Update it
+            OnColorChange(null, null);
         }
         #endregion
 
