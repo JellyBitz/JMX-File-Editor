82408cb0f151933ff8ee1a7a2f1c648c2f34d898
Engels Quintero <engelsquintero@gmail.com>
2023-08-14 00:51:38 -0400

    Fixed some graph stuff Added missing types to PrimAnimationType Improved all options sorted by string value

--- JMX File Editor/ViewModels/UserControls/ChartXYEditorVM.cs
+++ JMX File Editor/ViewModels/UserControls/ChartXYEditorVM.cs
@@ -1,4 +1,5 @@
 ﻿using JMXFileEditor.Silkroad.Data.JMXVENVI;
+using JMXFileEditor.Silkroad.Mathematics;
 using LiveCharts;
 using LiveCharts.Defaults;
 using LiveCharts.Wpf;
@@ -13,10 +14,10 @@ namespace JMXFileEditor.ViewModels
     public class ChartXYEditorVM : JMXProperty
     {
         #region Public Properties
-        public double MinValue { get; }
-        public double MaxValue { get; }
+        public double MinValue { get; private set; } = 0;
+        public double MaxValue { get; private set; } = 1;
         public SeriesCollection Series { get; } = new SeriesCollection();
-        public double Step { get; } = 0.2;
+        public double Step { get; } = 0.25;
         public Func<double, string> Formatter => value => value.ToString("N2");
         #endregion
 
@@ -28,39 +29,60 @@ namespace JMXFileEditor.ViewModels
         #region Constructors
         public ChartXYEditorVM(string name, List<EnvironmentFloatBlend> blends) : base(name, true)
         {
+            // Default values
+            if (blends.Count == 0)
+            {
+                blends.Add(new EnvironmentFloatBlend() { Time = 0, Value = 1});
+                blends.Add(new EnvironmentFloatBlend() { Time = 1, Value = 1 });
+            }
             // Cast values into ChartValues
             var values = new ChartValues<ObservablePoint>();
             foreach (var v in blends)
-            {
                 values.Add(new ObservablePoint(v.Time, v.Value));
-                // Set up min/max
-                if (MaxValue <= v.Time)
-                    MaxValue = v.Time;
-                if (MinValue > v.Time)
-                    MinValue = v.Time;
+            Initialize(values);
+        }
+        public ChartXYEditorVM(string name,List<Vector2> graph) : base(name, true)
+        {
+            // Default values
+            if(graph.Count == 0)
+            {
+                graph.Add(new Vector2(0,0));
+                graph.Add(new Vector2(1,1));
             }
-            // Create Line
-            Series.Add(new LineSeries() {
-                Values = values
-            });
-
-            InitializeCommands();
+            // Cast values into ChartValues
+            var values = new ChartValues<ObservablePoint>();
+            foreach (var v in graph)
+                values.Add(new ObservablePoint(v.X, v.Y));
+            Initialize(values);
         }
         #endregion
 
         #region Public Methods
         public List<EnvironmentFloatBlend> GetEnvironmentFloatBlends()
         {
-            List<EnvironmentFloatBlend> result = new List<EnvironmentFloatBlend>();
+            var result = new List<EnvironmentFloatBlend>();
             foreach (var v in Series[0].Values.Cast<ObservablePoint>())
                 result.Add(new EnvironmentFloatBlend() { Time = (float)v.X, Value = (float)v.Y });
+            return result;
+        }
+        public List<Vector2> GetVector2()
+        {
+            var result = new List<Vector2>();
+            foreach (var v in Series[0].Values.Cast<ObservablePoint>())
+                result.Add(new Vector2((float)v.X, (float)v.Y));
             return result;
         }
         #endregion
 
         #region Private Helpers
-        private void InitializeCommands()
+        private void Initialize(ChartValues<ObservablePoint> values)
         {
+            // Create Line
+            Series.Add(new LineSeries()
+            {
+                Values = values
+            });
+            // Initialize Commands
             CommandAddValue = new RelayCommand(() =>
             {
                 Series[0].Values.Add(new ObservablePoint() { X = 1, Y = 1});
